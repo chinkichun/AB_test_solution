29a8c71e10bc63654c87bc65b9de6ab2515ca874
frank <frank@justdice.io>
2023-10-20 11:21:30 +0200

    first commit

---
+++ experiment_solution/experiments_solution_functions.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import pingouin as pg #for t-Test
+import os
+import scipy.stats
+import itertools
+
+
+# In[2]:
+
+
+#Get averages of all metrics per test group
+
+def group_averages(df):
+    # group the data by the specified group column
+    groups = df.groupby('Test Group Id')
+    
+    # create a new dataframe to store the averages
+    averages = pd.DataFrame()
+    
+    # loop through all columns in the original dataframe
+    for col in df.columns:
+        # skip the group column
+        if col == 'Test Group Id':
+            continue
+            
+        elif col == 'Date':
+            continue
+            
+        elif col == 'Country':
+            continue
+        
+        else:
+        # calculate the average for each group
+            col_averages = groups[col].mean()
+
+            # add the column to the new dataframe, with the group as the row index
+            # averages[col] = col_averages.round(2)
+            averages[col]= col_averages.round(2).apply(lambda x: str(x).rstrip('0').rstrip('.') if '.' in str(x) else str(x))
+    
+    # transpose the new dataframe so that the columns become rows and vice versa
+    averages = averages.transpose()
+    averages.columns = pd.Index([f"Group {idx}" for idx in averages.columns], name='Test Group Id')
+    
+    return averages
+
+
+# In[3]:
+
+
+#Pivot df for further processing - ftest and ttest
+
+def pivot_df(df):
+    # Define the index and column names for the pivoted DataFrames
+    index = 'Date'
+    columns = 'Test Group Id'
+    
+    # Create a dictionary to store the pivoted DataFrames
+    
+    global pivoted_dfs
+    pivoted_dfs = {}
+    
+    # Loop through each column in the original DataFrame
+    for col in df.columns:
+        
+        if col == 'Date':
+            continue # also add skip 'country' column here
+        
+        elif col == 'Country':
+            continue
+            
+        elif col == 'Test Group Id':
+            continue
+            
+        else:
+            # Pivot the column as values against the index and columns
+            pivoted_col = df.pivot(index=index, columns=columns, values=col)
+            pivoted_col = pivoted_col.reset_index()
+            # Store the pivoted DataFrame in the dictionary
+#             pivoted_dfs[f"pivot_df_{col}"] = pivoted_col
+            pivoted_dfs[f"{col}"] = pivoted_col
+    
+    # Return the dictionary of pivoted DataFrames
+    
+    
+    return pivoted_dfs
+
+
+
+# In[4]:
+
+
+#Define F-Test function.
+
+#comment our irrelevant info
+
+
+#define F-Test function
+def f_test(group1, group2):
+    f = np.var(group1, ddof=1)/np.var(group2, ddof=1)
+    nun = group1.size-1
+    dun = group2.size-1
+    p_value = 1-scipy.stats.f.cdf(f, nun, dun)
+    
+    global p_value_2t
+    p_value_2t = p_value*2
+    
+#     print('F-score is: {} '.format(f))
+#     print('2 tailed p-Value is: {} '.format(p_value_2t))
+    
+    
+    global correction
+    if p_value_2t <= 0.05:
+        correction = True
+#         print ('Reject F-test Null hypothesis and assume unequal variance btw samples')
+    
+    else:
+        correction = False
+#         print ('Fail to reject F-test Null hypothesis and assume equal variance btw samples')
+
+
+# In[6]:
+
+
+#define t_test function 
+
+# make p-val dynamic so that users can select their own value
+
+correction = False
+
+def t_test(gp1, gp2, alternative='two-sided', correction=correction): #other options ['less', 'greater']
+    tTest = pg.ttest(gp1, gp2, alternative = alternative, correction = correction)
+    
+    gp1_average = gp1.mean().round(2)
+    gp2_average = gp2.mean().round(2)
+    
+#     print('gp1 average is: {} '.format(gp1_average))
+#     print('gp2 average is: {} '.format(gp2_average))
+#     print(tTest)
+    
+#     global tTest_df
+#     tTest_df = tTest
+    
+    p_val = tTest.at['T-test', 'p-val']
+    p_val = p_val
+    
+    print('t-test p_value is: {} '.format(tTest.at['T-test', 'p-val']))
+
+    if p_val <= 0.2: #80% confidence level #make this a variable instead of hard coded
+        return 'STATISTICAL DIFFERENCE between group {} (mean = {}) and group {} (mean = {}) with p-value of {}'.format(gp1.name, gp1_average, gp2.name, gp2_average, p_val)
+
+    else:
+        return 'No statistical difference between group {} (mean = {}) and group {} (mean = {}) with p-value of {}'.format(gp1.name, gp1_average, gp2.name, gp2_average, p_val)
+# In[7]:
+
+
+#Stat test function - combines f_test and t_test
+
+def stat_test(pivoted_dfs):
+    
+    t_test_results = []
+    
+    for key in pivoted_dfs.keys():
+        df = pivoted_dfs[key]
+    
+        # Ignore if the "Date" column is present in the current dataframe
+        if 'Date' in df.columns:
+            cols = [c for c in pivoted_dfs[key].columns if c != 'Date']
+        else:
+            cols = pivoted_dfs[key].columns
+
+        # Generate all possible pairs of column names
+        column_pairs = list(itertools.combinations(cols, 2))
+
+        for pair in column_pairs:
+            col1, col2 = pair
+#             print(f"Test results for {col1} vs {col2}")
+            f_test(df[col1], df[col2])
+            t_test_result = t_test(df[col1], df[col2])
+            t_test_results.append({'Metric': '{} - group {} vs group {}'.format(key, col1, col2), 
+                                   'Result': t_test_result})
+    
+    global df_results
+    df_results = pd.DataFrame(t_test_results)
+    df_results.index = df_results.index + 1
+    return df_results
+
+
+
+# In[ ]:
+
+
+
+
---
+++ experiment_solution/streamlit_app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+
+import pandas as pd
+import numpy as np
+import pingouin as pg #for t-Test
+import os
+import scipy.stats
+import os
+import itertools
+
+from experiments_solution_functions import group_averages, pivot_df, f_test, t_test, stat_test
+
+correction = False  #default correction before f-Test
+
+# Set page title
+st.set_page_config(page_title='Statistical Test Solution', layout = 'wide')
+
+# Define a function to read a CSV file and return a DataFrame
+def read_csv(file):
+    df = pd.read_csv(file)
+    return df
+
+
+
+def bg_color(result):
+    color = '#66ff99' if 'STATISTICAL DIFFERENCE' in result else ''
+    return f'background-color: {color}'
+
+# Define the Streamlit app
+def main():
+    # Set app title
+    st.markdown(
+        f"<h1 style='text-align: left; color: green;'>justDice Statistical Test Solution</h1>",
+        unsafe_allow_html=True
+    )
+
+    # st.title('Statistical Test Solution')
+    
+    
+    # Create a file uploader
+    file = st.file_uploader("Upload a CSV file", type=["csv"])
+    
+    # Add a "Go" button
+    if st.button('Run Test'):
+        # If the "Go" button is clicked and a file was uploaded, read it into a DataFrame
+        if file is not None:
+            df = read_csv(file)
+
+            st.subheader('Daily Averages')
+
+            averages = group_averages(df)
+            st.write(averages.style.set_table_attributes('style="width: 100%;"'))
+            # st.table(averages)
+
+            st.subheader('Test Results')
+
+            pivoted_df = pivot_df(df)
+            # Process the DataFrame and display the results
+            stat_test_result = stat_test(pivoted_df)
+            # st.write(stat_test_result.style.set_table_attributes('style="width: 100%;"'))
+            # st.table(stat_test_result)
+            st.table(stat_test_result.style.applymap(bg_color, subset=['Result']))
+
+
+
+
+if __name__ == '__main__':
+    main()
